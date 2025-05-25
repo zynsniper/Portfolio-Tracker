dad227e85059a460bdb07a001ead4b80c24d2943
zynsniper <cheang5337@gmail.com>
2025-05-25 03:17:54 -0300

    Added input handling for asset(name/quantity/price)

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+portfolioData = []
+
+@app.route("/", methods = ["GET", "POST"])
 def home():
-    return render_template("home.html")
+    if request.method == "POST":
+        asset = request.form.get("asset")
+        quantity = request.form.get("quantity")
+        price = request.form.get("price")
+
+        return redirect(url_for("home"))
+
+    return render_template("home.html", portfolio=portfolioData)
 
 if __name__ == "__main__":
     app.run(debug=True)
