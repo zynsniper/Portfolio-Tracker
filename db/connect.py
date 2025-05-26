468947a93a4534b88c2a40acc7e27c5fc6ac9917
zynsniper <cheang5337@gmail.com>
2025-05-26 18:40:47 -0300

    connect function now refers to database.ini for credentials

--- db/connect.py
+++ db/connect.py
@@ -1,17 +1,8 @@
 import psycopg2
 from db.config import load_config
 
-def connect(config):
-    """ Connect to the PostgreSQL database server """
-    try:
-        # connecting to the PostgreSQL server
-        with psycopg2.connect(**config) as conn:
-            print('Connected to the PostgreSQL server.')
-            return conn
-    except (psycopg2.DatabaseError, Exception) as error:
-        print(error)
-
-
-if __name__ == '__main__':
+def connect():
     config = load_config()
-    connect(config)
+    conn = psycopg2.connect(**config)
+
+    return conn
